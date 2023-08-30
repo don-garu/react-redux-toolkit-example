b761f677609c57f3ddb398d6261243ead2186aff
don-garu <hwangji9637@gmail.com>
2023-08-30 14:31:26 +0900

    3 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
 import './style.css';
+import React from 'react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import store from './store.js';
+import { up } from './counterSlice.js';
 
-export default function App() {
+function Counter() {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <button
+        onClick={() => {
+          dispatch(up(2));
+        }}
+      >
+        +
+      </button>
+      {count}
     </div>
   );
 }
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div>
+        <Counter></Counter>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
